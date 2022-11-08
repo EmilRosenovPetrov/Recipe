6433f8e15ce6bb6ac4c0a8a8bc856ebadb0ebfc0
Emil Petrov <emil.petrov2020@gmail.com>
2022-11-08 18:11:28 +0200

    Add project files.

---
+++ Services/Recipe.Services.Data/SettingsService.cs
@@ -0,0 +1,29 @@
+﻿namespace Recipe.Services.Data
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using Recipe.Data.Common.Repositories;
+    using Recipe.Data.Models;
+    using Recipe.Services.Mapping;
+
+    public class SettingsService : ISettingsService
+    {
+        private readonly IDeletableEntityRepository<Setting> settingsRepository;
+
+        public SettingsService(IDeletableEntityRepository<Setting> settingsRepository)
+        {
+            this.settingsRepository = settingsRepository;
+        }
+
+        public int GetCount()
+        {
+            return this.settingsRepository.AllAsNoTracking().Count();
+        }
+
+        public IEnumerable<T> GetAll<T>()
+        {
+            return this.settingsRepository.All().To<T>().ToList();
+        }
+    }
+}
